188e31d6fa2e80225d300a47ccb1fcb2f5faccc0
Hector Salgado <53808824+hector323@users.noreply.github.com>
2020-12-08 21:46:11 -0800

    updated current files

--- build.py
+++ build.py
@@ -11,6 +11,7 @@ pages = [
 import glob
 
 all_html_files = glob.glob("content/*.html")
+print("-------- these are all the html files --------")
 print(all_html_files)
 
 # 2.1.2
@@ -18,7 +19,9 @@ print(all_html_files)
 import os
 file_path = "content/blog.html"
 file_name = os.path.basename(file_path)
+print("-------- this is the file name from the path ------")
 print(file_name)
+print("--------")
 name_only, extension = os.path.splitext(file_name)
 print(name_only)
 
@@ -30,6 +33,7 @@ pages.append({
   "title": "Index",
   "output": "docs/index.html",
 })
+print("--------")
 print(pages)
 
 # 2.1.4 Phase Summary
@@ -68,34 +72,35 @@ template.render(
 # Get the main base template
 # First, get the template files
 # template = open('./templates/base.html').read()
-def file_content(page_title):
-  base_template = open("./templates/base.html").read()
-  # replace title with page_title
-  template = base_template.replace("{{title}}", page_title)
-  return template
 
-# Read in index HTML code
-# needs both template and content pages
-def apply_template(template, content):
-  # apply content page with template with using a placeholder {{content}} in the body
-  base_template = open("./templates/base.html").read()
-  completed_page = template.replace("{{content}}", content)
-  return completed_page
+# def file_content(page_title):
+#   base_template = open("./templates/base.html").read()
+#   # replace title with page_title
+#   template = base_template.replace("{{title}}", page_title)
+#   return template
 
-# loop through each page
-def main():
-  for page in pages:
-    page_content = page['filename']
-    page_output = page['output']
-    page_title = page['title']
+# # Read in index HTML code
+# # needs both template and content pages
+# def apply_template(template, content):
+#   # apply content page with template with using a placeholder {{content}} in the body
+#   base_template = open("./templates/base.html").read()
+#   completed_page = template.replace("{{content}}", content)
+#   return completed_page
 
-    # Read in the entire template
-    template = file_content(page_title)
-    content = open(page_content).read()
-    # Read in the content of the index HTML page
-    completed_page = apply_template(template, content)
-    open(page_output, "w+").write(completed_page)
+# # loop through each page
+# def main():
+#   for page in pages:
+#     page_content = page['filename']
+#     page_output = page['output']
+#     page_title = page['title']
 
-# invoke the main function
-if __name__ == "__main__":
-    	main()
+#     # Read in the entire template
+#     template = file_content(page_title)
+#     content = open(page_content).read()
+#     # Read in the content of the index HTML page
+#     completed_page = apply_template(template, content)
+#     open(page_output, "w+").write(completed_page)
+
+# # invoke the main function
+# if __name__ == "__main__":
+#     	main()
